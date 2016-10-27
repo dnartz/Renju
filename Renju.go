35f394040a0fc54e556e364ac1b42af4c75817fd
liutao <xylon.tao@gmail.com>
2016-10-28 00:41:19 +0800

    Runable HTTP server

--- Renju.go
+++ Renju.go
@@ -7,9 +7,35 @@ import "C"
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	var cmd map[string]*json.RawMessage
+
+	json.Unmarshal(body, &cmd)
+	json.Unmarshal(*cmd["body"], cmd)
+
+	var str string
+	json.Unmarshal(*cmd["url"], &str)
+	fmt.Printf("%s", str)
+
+	fmt.Fprintf(w, "test")
+}
+
 func main()  {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":80", nil))
+
 	var i, j int
 
 	for (C.win == 0) {
